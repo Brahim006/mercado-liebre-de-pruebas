cfce9b25d38f728f653172dcad642bbbd6fbd170
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-08-27 16:31:57 -0300

    Creado el middleware para restringir el acceso a rutas para las que se necesite tener un usuario tipo 'vendedor'

---
+++ src/middleWares/sellerRoute.js
@@ -0,0 +1,12 @@
+/**
+ * Middleware para controlael acceso a rutas qué sólo deban ser accedidas por usuarios
+ * con el perfil de "vendedor"
+ */
+
+module.exports = function(req, res, next){
+    if(req.session.user == 1){
+        next();
+    } else {
+        res.redirect("/products");
+    }
+}
