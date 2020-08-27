cc33838953fa0da435eac5b4b45d9fe68185f7d2
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-08-27 16:32:45 -0300

    Implementados los middlewares de control de acceso a rutas según la autenticación del usuario

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router =  express.Router();
 const controller = require("../controllers/usersControllers");
 const dataTrimmer = require("../middleWares/dataTrimmer");
+const guestRoute = require("../middleWares/guestRoute");
+const userRoute = require("../middleWares/userRoute");
 
 // Trimmers
 const registerDataTrimmer = dataTrimmer("name", "lastName", "userName", "email", "address");
@@ -11,12 +13,12 @@ const loginDataTrimmer = dataTrimmer("userName");
 const registerValidator = require("../validators/registerValidator");
 const loginValidator = require("../validators/loginValidator");
 
-router.get("/register", controller.register);
+router.get("/register", guestRoute, controller.register);
 router.post("/register", registerDataTrimmer, registerValidator, controller.store);
 
-router.get("/login", controller.login);
+router.get("/login", guestRoute, controller.login);
 router.post("/login", loginDataTrimmer, loginValidator, controller.athenticate);
 
-router.get("/logout", controller.logout);
+router.get("/logout", userRoute, controller.logout);
 
 module.exports = router;
